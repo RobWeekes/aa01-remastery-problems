508c85ccd1e7ef5450a4304f555af0f235937a38
Robert Weekes <weekes.robert@gmail.com>
2024-03-24 21:57:54 -0700

    done w problem 6

--- problems/06-two-sum.js
+++ problems/06-two-sum.js
@@ -1,11 +1,25 @@
 /*
 Write a function `twoSum(arr, target)` that accepts an array and a target number
-as args. The function should a return a boolean indicating if two distinct
+as args. The function should return a boolean indicating if two distinct
 numbers of the array add up to the `target` value. You can assume that input
 array contains only unique numbers.
 */
 
-// Your code here 
+let twoSum = function(arr, target) {
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            sum = sum + arr[i] + arr[j];
+            // console.log(sum);
+            if(sum === target) {
+                return true;
+            }
+            sum = 0;
+        }
+    }
+    return false;
+}
 
 console.log(twoSum([1, 7, 3, 0, 2], 5)); // true
 console.log(twoSum([1, 7, 3, 0, 2], 6)); // false
