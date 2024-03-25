cad459ea6d0a651217ce60ce6c46a8db79d016b5
Robert Weekes <weekes.robert@gmail.com>
2024-03-24 22:14:38 -0700

    done w problem 8

--- problems/08-for-loop-translation.js
+++ problems/08-for-loop-translation.js
@@ -7,16 +7,14 @@ forget the syntax for a `for` loop go back and check out the reading.
 */
 
 function aCounter(word) {
-    let index = 0;
     let count = 0;
 
     // change this while loop into a for loop
-    while (index < word.length) {
+    for(let index = 0; index < word.length; index++) {
         let char = word[index];
         if (char === "a" || char === "A") {
             count += 1;
         }
-        index++;
     }
     return count;
 };
