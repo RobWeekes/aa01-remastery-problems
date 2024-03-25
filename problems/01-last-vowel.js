8248f2492769615a0bb2b1bd38025c8ebe68ada7
Robert Weekes <weekes.robert@gmail.com>
2024-03-24 20:21:42 -0700

    done with problem 1

--- problems/01-last-vowel.js
+++ problems/01-last-vowel.js
@@ -9,16 +9,26 @@ String.toLowerCase: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Refe
 String.toUpperCase: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
 
-
-// Your code here
-/// add test code to commit
-
+let vowels = 'aeiouAEIOU';
+function lastVowel(str) {
+    str = str.split('');
+    for(let i = str.length - 1; i > -1; i--) {
+        if(vowels.includes(str[i])) {
+            console.log(str[i]);
+            return str[i];
+        } else if(i == 0) {
+            return null;
+        }
+    }
+}
 
 console.log(lastVowel('battery')); // 'e'
 console.log(lastVowel('TUNNEL')); // 'E'
 console.log(lastVowel('dog')); // 'o'
 console.log(lastVowel('conventional')); // 'a'
 console.log(lastVowel('rhythm')); // null
+
+console.log(lastVowel('pipe'));
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
