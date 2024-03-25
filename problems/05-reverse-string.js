a713550b267aa3a1d5e12b425fa56b9c671b426f
Robert Weekes <weekes.robert@gmail.com>
2024-03-24 21:42:46 -0700

    done w problem 5

--- problems/05-reverse-string.js
+++ problems/05-reverse-string.js
@@ -3,7 +3,23 @@ Write a function `reverseString(str)` that takes in a string. The function
 should return a new string where the order of the characters is reversed.
 */
 
-// Your code here 
+function reverseString(str) {
+    str = str.split('');
+
+    // quick way with reverse method
+    // newStr = str.reverse();
+    // // console.log(newStr)
+    // newStr = newStr.join('');
+    // return newStr;
+
+    let otherStr = [];
+    for(let i = 0; i < str.length; i++) {
+        otherStr.unshift(str[i]);
+        // console.log(otherStr);
+    }
+        otherStr = otherStr.join('');
+        return otherStr;
+}
 
 console.log(reverseString('fish')); // 'hsif'
 console.log(reverseString('marathon')); // 'nohtaram'
