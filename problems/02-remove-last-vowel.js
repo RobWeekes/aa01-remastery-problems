1d8c147a027152d6e836b76004a17f26b4dff6d8
Robert Weekes <weekes.robert@gmail.com>
2024-03-24 21:14:29 -0700

    done with problem 2

--- problems/02-remove-last-vowel.js
+++ problems/02-remove-last-vowel.js
@@ -3,7 +3,22 @@ Write a function `removeLastVowel(word)` that takes in a string and returns the
 string with its last vowel removed.
 */
 
-// Your code here 
+let vowels = 'aeiouAEIOU';
+
+function removeLastVowel(word) {
+    word = word.split('');
+    for(let i = word.length - 1; i > -1; i--) {
+        if(vowels.includes(word[i])) {
+            // console.log(word[i]);
+            word[i] = '';
+            word = word.join('');
+            return word;
+        } else
+        if(i == 0) {
+            return word.join('');
+        }
+    }
+}
 
 console.log(removeLastVowel('bootcamp')); // 'bootcmp'
 console.log(removeLastVowel('better')); // 'bettr'
