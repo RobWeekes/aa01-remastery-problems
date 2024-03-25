0958a0fb361c82fe27c4b49ef748c15ac85df1ae
Robert Weekes <weekes.robert@gmail.com>
2024-03-24 22:30:44 -0700

    done with problem 9

--- problems/09-is-prime.js
+++ problems/09-is-prime.js
@@ -3,13 +3,26 @@ Define a function `isPrime(number)` that returns `true` if `number` is prime.
 Otherwise, false. A number is prime if it is only divisible by 1 and itself.
 */
 
-// Your code here 
+function isPrime(number) {
+    if(number <= 1) {
+        return false;
+    }
+    for(let i = 2; i < number; i++) {
+        if(number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
 console.log(isPrime(2)); // => true
 console.log(isPrime(10)); // => false
 console.log(isPrime(11)); // => true
 console.log(isPrime(9)); // => false
 console.log(isPrime(2017)); // => true
+
+console.log(isPrime(0));
+console.log(isPrime(1));
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
